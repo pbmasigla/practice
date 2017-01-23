24bec59315a44e6bff0c7109c22e5215c352908d
Patricia Masigla <patricia@socialtables.com>
2017-01-22 23:24:00 -0500

    more problems

--- 2 - arrays and strings.js
+++ 2 - arrays and strings.js
@@ -82,3 +82,62 @@ const easyUrlify = (string: string, stringLength: number) => {
 	}
 	return stringArray.join("");
 };
+
+
+//1.4
+
+const isPermutationPalindrome = (word: string) => {
+	const letterCounts = getLetterCounts(word.replace(" ", ""));
+
+	const oddCount = Object.values(letterCounts).reduce((count, letterCount) => {
+		if (letterCount % 2 === 1) {
+			count++;
+		}
+		return count;
+	}, 0);
+
+	return oddCount <= 1;
+};
+
+
+
+//1.5
+
+const isOneEdit = (firstString: string, secondString: string) => {
+	if (Math.abs(firstString.length - secondString.length) <= 1) {
+		let isEdited = true;
+		let numChanges = 0;
+		for (let i = 0, j = 0; i < firstString.length && j < secondString.length; i++, j++) {
+			if (isEdited && firstString[i] !== secondString[j]) {
+				numChanges++;
+				if (firstString.length < secondString.length) {
+					j++;
+				}
+				else if (firstString.length > secondString.length) {
+					j--;
+				}
+			}
+		}
+		return isEdited && numChanges <= 1;
+	}
+	return false;
+};
+
+
+
+// 1.6
+
+const compressString = (string: string) => {
+	let newString = "";
+	let index = 0;
+	while (index < string.length) {
+		let checkIndex = index + 1;
+		while (string[index] === string[checkIndex]) {
+			checkIndex++;
+		}
+
+		newString = `${newString}${string[index]}${checkIndex - index}`;
+		index = checkIndex;
+	}
+	return newString;
+};
